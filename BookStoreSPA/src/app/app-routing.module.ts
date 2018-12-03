959cf8d9600e1ff252fc64fd219bc1b8d83166cb
Austin Gilmore <fizzled10@gmail.com>
2018-12-02 21:38:26 -0600

    Updated home page and added multiple components, changed some styling on add book

--- BookStoreSPA/src/app/app-routing.module.ts
+++ BookStoreSPA/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddBookComponent } from './addBook/addBook.component';
 import { BookComponent } from './book/book.component';
+import {HomePageComponent} from './home-page/home-page.component';
 
 
 const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
   {path: 'add', component: AddBookComponent},
   {path: 'books', component: BookComponent}
 ];
